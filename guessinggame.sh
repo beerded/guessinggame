0670f66e82de0aea16f1505aa08565c1d9c37eb4
Adam P <beerded@users.noreply.github.com>
2020-07-08 00:01:19 +0300

    Guessing game script is done
    Program doesn't handle non-integer input in a clever way, but it works
    well enough.

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+CORRECT_NFILES=$(ls | wc -l | awk '{$1=$1};1')
+GUESSED=false
+
+function guess {
+    read GUESSED_NFILES
+    if [ $GUESSED_NFILES -eq $CORRECT_NFILES ]; then
+        echo "Congratulations. You're right. You win."
+        GUESSED=true
+    elif [ $GUESSED_NFILES -lt $CORRECT_NFILES ]; then
+        echo "Too low. Guess again."
+    else
+        echo "Too high. Guess again."
+    fi
+}
+
+echo "Step right up! How many files do you think are in this directory?"
+while [ $GUESSED == false ]; do
+    guess
+done
+
+echo "There are $CORRECT_NFILES files in this directory"
